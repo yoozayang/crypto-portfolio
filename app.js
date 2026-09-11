debd0ca3dcb4b4905b6171aa05a09ceaf0006612
yoozayang <36257301+yoozayang@users.noreply.github.com>
2026-09-11 14:54:09 +0800

    Refresh holding card display cache

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import './cloud-overlay.js?v=20260911-2';
 import {loadSeed,loadState,saveState} from './records.js?v=20260907-1545';
-import {mountApp} from './display.js?v=20260911-1415';
+import {mountApp} from './display.js?v=20260911-1425';
 
 (async()=>{
   await window.cryptoPortfolioCloudReady;
